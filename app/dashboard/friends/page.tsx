070cfdd721027a2ad870ec0256c2d3ca49234bc6
Kristin Diep <kristiyip@gmail.com>
2024-10-03 16:32:31 -0400

    setup app and add side nav

---
+++ app/dashboard/friends/page.tsx
@@ -0,0 +1,19 @@
+// Should show all friends and their status(offline/online)
+// Pending friend requests
+//
+// Data structure for friend list:
+// [
+//   {
+//     id: 1,
+//     name: 'kittykity',
+//     status: 'online'
+//   }
+// ]
+
+export default function Page() {
+  return (
+    <div>
+      Friends
+    </div>
+  )
+}
---
+++ app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,7 @@
+export default function SideNav() {
+  return (
+    <div className="flex flex-col h-full px-3 bg-slate-400 border-r">
+      Here
+    </div>
+  )
+}
